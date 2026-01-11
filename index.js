0bc7a5fe6a6eef4403e719311305c51ee304c57c
Developer RS | Rishabh Sharma <164728448+developer-rs5@users.noreply.github.com>
2026-01-11 18:26:09 +0530

    .

--- index.js
+++ index.js
@@ -1,7 +1,9 @@
 const express = require('express')
 const mineflayer = require('mineflayer')
+const cors = require("cors")
 
 const app = express()
+app.use(cors())
 app.use(express.json())
 
 const bots = {}
